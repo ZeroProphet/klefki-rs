1aac3f72e641a8adf9421365e807e6cfa3c70a0e
Ryan.K <ryankung@ieee.org>
2019-01-31 18:57:27 +0800

    some prepare

---
+++ src/impls/mod.rs
@@ -0,0 +1,2 @@
+pub mod ecg;
+pub mod consts;
--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,10 @@
+#[cfg(feature="std")]
+extern crate core;
+#[macro_use]
+extern crate uint;
+#[macro_use]
+extern crate lazy_static;
+extern crate hex;
+
 pub mod types;
+pub mod impls;
