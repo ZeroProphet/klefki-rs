6514d0cdadfcd0d9f6e72b86c80eb31ff3e41ec6
Ryan.K <ryankung@ieee.org>
2021-01-25 19:58:42 +0800

    impl ecg for Add

--- src/algebra/groups/ecg.rs
+++ src/algebra/groups/ecg.rs
@@ -1,33 +1,36 @@
 use crate::algebra::fields::prime;
-use std::ops::{Add, Sub};
+use std::ops::Add;
 
 
 #[allow(non_snake_case)]
-pub struct CurveParams<T> {
-    pub A: prime::PrimeField<T>,
-    pub B: prime::PrimeField<T>
+pub struct CurveParams<F> {
+    pub A: prime::PrimeField<F>,
+    pub B: prime::PrimeField<F>
 }
 
-#[allow(non_snake_case)]
-pub struct CurvePoint<T> {
-    pub X: prime::PrimeField<T>,
-    pub Y: prime::PrimeField<T>
+pub struct CurvePoint<F> {
+    pub x: prime::PrimeField<F>,
+    pub y: prime::PrimeField<F>
 }
 
-pub trait New<F> {
-    fn new(point: CurvePoint<F>);
+pub trait Curve<F, T> {
+    fn new(x: prime::PrimeField<F>, y: prime::PrimeField<F>) -> Box<dyn EllipticCurveGroupProperty<F, T>>;
+    fn add(a: CurvePoint<F>, b: CurvePoint<F>) -> Box<dyn EllipticCurveGroupProperty<F, T>>;
 }
 
-pub trait EllipticCurveGroupProperty<T, F> {
-    fn params(&self) -> CurveParams<T>;
+pub trait EllipticCurveGroupProperty<F, T>
+where T: Curve<F, T>, F: prime::New
+{
+    fn point(&self) -> CurvePoint<F>;
 }
 
-pub type EllipticCurveGroup<T, F> = Box<dyn EllipticCurveGroupProperty<T, F>>;
+pub type EllipticCurveGroup<F, T> = Box<dyn EllipticCurveGroupProperty<F, T>>;
 
-// impl <T, F> Add for EllipticCurveGroup<T, F>
-//     where T: New<F>
-// {
-//     type Output = Self;
-//     fn add(self, rhs: Self) -> Self {
-//     }
-// }
+impl <F, T> Add for EllipticCurveGroup<F, T>
+    where T: Curve<F, T>, F: prime::New
+{
+    type Output = Self;
+    fn add(self, rhs: Self) -> Self {
+        return T::add(self.point(), rhs.point())
+    }
+}
