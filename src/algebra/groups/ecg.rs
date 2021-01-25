0a69cc45e101ab2f51603eb7d6ed9f9c41c2d718
Ryan.K <ryankung@ieee.org>
2021-01-25 22:00:26 +0800

    ecg

--- src/algebra/groups/ecg.rs
+++ src/algebra/groups/ecg.rs
@@ -1,6 +1,9 @@
 use crate::algebra::fields::prime;
-use std::ops::Add;
-
+use crate::algebra::traits::Group;
+use std::ops::{Add, Sub};
+use std::ops::Neg;
+use num::traits::Zero;
+use std::cmp::{Eq, PartialEq};
 
 #[allow(non_snake_case)]
 pub struct CurveParams<F> {
@@ -8,29 +11,84 @@ pub struct CurveParams<F> {
     pub B: prime::PrimeField<F>
 }
 
+
 pub struct CurvePoint<F> {
     pub x: prime::PrimeField<F>,
     pub y: prime::PrimeField<F>
 }
 
-pub trait Curve<F, T> {
-    fn new(x: prime::PrimeField<F>, y: prime::PrimeField<F>) -> Box<dyn EllipticCurveGroupProperty<F, T>>;
-    fn add(a: CurvePoint<F>, b: CurvePoint<F>) -> Box<dyn EllipticCurveGroupProperty<F, T>>;
+pub trait Curve<F, G> {
+    fn new(x: prime::PrimeField<F>, y: prime::PrimeField<F>) -> Box<dyn EllipticCurveGroupProperty<F, G>>;
+    fn add(a: CurvePoint<F>, b: CurvePoint<F>) -> Box<dyn EllipticCurveGroupProperty<F, G>>;
 }
 
-pub trait EllipticCurveGroupProperty<F, T>
-where T: Curve<F, T>, F: prime::New
+pub trait EllipticCurveGroupProperty<F, G>
+where G: Curve<F, G>, F: prime::New
 {
     fn point(&self) -> CurvePoint<F>;
 }
 
-pub type EllipticCurveGroup<F, T> = Box<dyn EllipticCurveGroupProperty<F, T>>;
+pub type EllipticCurveGroup<F, G> = Box<dyn EllipticCurveGroupProperty<F, G>>;
 
-impl <F, T> Add for EllipticCurveGroup<F, T>
-    where T: Curve<F, T>, F: prime::New
+impl <F, G> Add for EllipticCurveGroup<F, G>
+    where G: Curve<F, G>, F: prime::New
 {
     type Output = Self;
     fn add(self, rhs: Self) -> Self {
-        return T::add(self.point(), rhs.point())
+        return G::add(self.point(), rhs.point())
     }
 }
+
+impl <F, G> Zero for EllipticCurveGroup<F, G>
+where G: Curve<F, G>, F: prime::New
+{
+    fn zero() -> Self {
+        return G::new(prime::PrimeField::<F>::zero(), prime::PrimeField::<F>::zero());
+    }
+    fn is_zero(&self) -> bool {
+        return self.point().x == prime::PrimeField::<F>::zero() &&
+            self.point().y == prime::PrimeField::<F>::zero();
+    }
+
+}
+
+impl <F, G> Neg for EllipticCurveGroup<F, G>
+    where G: Curve<F, G>, F: prime::New
+{
+    type Output = Self;
+    fn neg(self) -> Self {
+        return G::new(
+            self.point().x,
+            -self.point().y
+        )
+    }
+}
+
+impl <F, G> Sub for EllipticCurveGroup<F, G>
+    where G: Curve<F, G>, F: prime::New
+{
+    type Output = Self;
+    fn sub(self, rhs: Self) -> Self {
+        return self + (- rhs);
+    }
+}
+
+impl <F, G> PartialEq for EllipticCurveGroup<F, G>
+where G: Curve<F, G>, F: prime::New
+{
+    fn eq(&self, rhs: &Self) -> bool {
+        return self.point().x == rhs.point().x &&
+            self.point().y == rhs.point().y
+    }
+
+    fn ne(&self, rhs: &Self) -> bool {
+         return self.point().x != rhs.point().x ||
+            self.point().y != rhs.point().y
+    }
+}
+
+impl <F, G> Eq for EllipticCurveGroup<F, G>
+where G: Curve<F, G>, F: prime::New {}
+
+impl <F, G> Group for EllipticCurveGroup<F, G>
+where G: Curve<F, G>, F: prime::New {}
