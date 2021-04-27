8c911abeddfd95680dadd69bbf809fa5ec39ab32
Ryan.K <ryankung@ieee.org>
2021-04-27 17:17:22 +0800

    refined u2556

--- src/algebra/rings/integer.rs
+++ src/algebra/rings/integer.rs
@@ -2,35 +2,33 @@ use std::ops::{Add, Div, Mul, Sub};
 use std::convert::TryFrom;
 use std::cmp::Eq;
 
-/// U256 on littleEdian
-/// ref: https://arxiv.org/ftp/arxiv/papers/1204/1204.0232.pdf
 
 #[derive(Debug, Clone, Copy)]
-pub struct U256([u64;6]);
+#[allow(non_camel_case_types)]
+pub struct u256([u64;6]);
 
-impl PartialEq for U256 {
+impl PartialEq for u256 {
     fn eq(&self, rhs: &Self) -> bool {
         return self.0 == rhs.0
     }
 }
 
-impl Add for U256 {
+impl Add for u256 {
     type Output = Self;
     fn add(self, rhs: Self) -> Self {
-        let mut carry: u128 = 0;
-        let mut sum: u128;
+        let mut sum: u64;
         let mut out = [0u64;6];
+        let mut overflow = false;
 
         for i in 0 .. 6 {
-            sum = u128::from(self.0[i]) + u128::from(rhs.0[i]) + carry;
-            carry = sum >> 64;
-            out[i] = u64::try_from(sum & u128::from(u64::MAX)).unwrap();
+            (sum, overflow) = (self.0[i] + (overflow as u64)).overflowing_add(rhs.0[i]);
+            out[i] = sum;
         }
         return Self(out);
     }
 }
 
-impl Sub for U256 {
+impl Sub for u256 {
     type Output = Self;
     fn sub(self, rhs: Self) -> Self {
         let mut borrow = 0;
@@ -51,14 +49,14 @@ impl Sub for U256 {
 mod tests {
     extern crate test;
 
-    use super::U256;
+    use super::u256;
     use rand::Rng;
     use test::Bencher;
 
     #[test]
     fn test_add_sub() {
         let mut rng = rand::thread_rng();
-        let a = U256(
+        let a = u256(
             [
                 rng.gen(),
                 rng.gen(),
@@ -68,7 +66,7 @@ mod tests {
                 rng.gen()
             ]
         );
-        let b = U256(
+        let b = u256(
             [
                 rng.gen(),
                 rng.gen(),
@@ -87,7 +85,7 @@ mod tests {
     #[bench]
     fn bench_add(b: &mut Bencher) {
         let mut rng = rand::thread_rng();
-        let a = U256(
+        let a = u256(
             [
                 rng.gen(),
                 rng.gen(),
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 #![feature(trait_alias)]
 #![feature(box_syntax)]
 #![feature(test)]
+#![feature(destructuring_assignment)]
 
 pub mod algebra;
 pub mod curves;
